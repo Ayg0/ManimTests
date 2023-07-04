041607d2e5f3bfcfca5892eaacd1fee6f18caa4b
Ayg0 <ted-dafi@student.1337.com>
2023-07-04 15:23:47 +0100

    ok

--- l.py
+++ l.py
@@ -1,5 +1,5 @@
 from manim import *
-
+from math import *
 class test(Scene):
 	def construct(self):
 		name = Tex("WhyNot").to_edge(UL, buff=0.7)
@@ -47,8 +47,31 @@ class Cirlewithregulator(Scene):
 		cirl = always_redraw(lambda: Circle(k.get_value(), color=BLUE, fill_color=DARK_BLUE, fill_opacity=0.5).scale(0.4).to_corner(LEFT, buff=1))
 		self.play(Create(num), run_time=2)
 		self.play(DrawBorderThenFill(box), DrawBorderThenFill(cirl), run_time=2)
-		self.play(k.animate.set_value(1), box.animate.rotate(PI / 3), run_time=5)
-		self.play(k.animate.set_value(5), box.animate.rotate(-PI / 3), run_time=5)
+		self.play(k.animate.set_value(1), box.animate.rotate(PI / 3), run_time=5, rate_function=smooth)
+		self.play(k.animate.set_value(5), box.animate.rotate(-PI / 3), run_time=5, rate_function=smooth)
 		self.play(Uncreate(cirl), Uncreate(box), run_time=2)
 		self.wait()
-		# self.play(num.animate.shift(RIGHT * 4), run_time=2)
+
+class graphing(Scene):
+	def construct(self):
+		plane = NumberPlane(x_range=[-1, 1, 0.25], x_length=7, y_range=[-10, 10, 10], y_length=8)
+		prob = plane.plot(lambda x : tan(x) / sin(x), x_range = (-1, 1) , color = GREEN)
+		self.play(DrawBorderThenFill(plane))
+		self.play(Create(prob))
+
+class circle(Scene):
+	def construct(self):
+		cir = Circle(2, BLACK, fill_color=DARK_BLUE, fill_opacity=0.75);
+		cir2 = Circle(1.5, DARK_BLUE, fill_color=BLACK, fill_opacity=1);
+		cir3 = Circle(0.05, BLACK, fill_color=BLACK, fill_opacity=1);
+		s1 = Line(start=LEFT * 2, end=RIGHT * 2)
+		s2 = Line(start=UP * 2, end=DOWN * 2)
+		s3 = Line(start=UL * 1.04, end=DR * 1.04)
+		s4 = Line(start=UR * 1.04, end=DL * 1.04)
+		# s4 = Line(start=UR * 1.04, end=DL * 1.04)
+		self.play(DrawBorderThenFill(cir), run_time=2)
+		self.play(Create(s1), Create(s2))
+		self.play(Create(s3), Create(s4))
+		self.play(Create(cir3))
+		self.play(DrawBorderThenFill(cir2), run_time=3)
+		self.wait()
